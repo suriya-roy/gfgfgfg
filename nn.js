7dfe5cc645cd68a7bc19b894a3fb599f5f0d530e
Saurya-raya <saurya7023@gmail.com>
2025-05-07 10:21:07 +0545

    first commit

---
+++ nn.js
@@ -0,0 +1,11 @@
+function hello () {
+    const inp=document.getElementById("name")
+    const Iname =document.getElementById("Iname")
+   Iname.innerHTML=inp.value
+
+    const parent= document.getElementById("parent")
+    parent.style.display="block"
+
+const dd=document.getElementById("dd")
+dd.style.display="block"
+}
